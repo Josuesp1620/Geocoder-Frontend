dadb7f7beaf2acad861bf777d32db432be0d65c4
Josuesp1620 <josuesp1620@gmail.com>
2024-11-14 14:12:16 +0100

    [Update] Refactor Project

--- src/core/main.tsx
+++ src/core/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '../assets/css/index.css'
+import { MapView } from '../views/Map/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MapView />
   </StrictMode>,
 )
