c890458eb21f19dfce9e2c473355b033ac70a967
Josuesp1620 <josuesp1620@gmail.com>
2024-11-14 14:23:52 +0100

    [Add] Library Maplibre Config

--- src/views/Map/index.tsx
+++ src/views/Map/index.tsx
@@ -1,5 +1,16 @@
+import Map from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 export const MapView = () => {
     return (
-        <>Map</>
+        <Map
+            initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 14
+            }}
+            style={{width: "100vw", height: "100vh"}}
+            mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
+        />
     );
 }
